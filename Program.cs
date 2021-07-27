83a75edd643526ee4a4a5c3ad8824985b3210a27
Camille Viot <viot.camille@outlook.com>
2021-07-27 21:18:00 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jclox
+{
+    class Program
+    {
+        public static int main(String[] args)
+        {
+            if (args.Length > 1) {
+                Console.WriteLine("Usage: jlox [script]");
+                return 64;
+            } else if (args.Length == 1) {
+                runFile(args[0]);
+            } else {
+                runPrompt();
+            }
+
+            return 0;
+        }
+
+        private static void runFile(String path) 
+        {
+            var text = System.IO.File.ReadAllText(path);
+            run(text);
+        }
+
+        private static void runPrompt()
+        {
+
+            for (;;) {
+                Console.WriteLine("> ");
+                string line = Console.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                run(line);
+
+                hadError = false;
+            }
+        }
+
+        static void error(int line, String message)
+        {
+            report(line, "", message);
+        }
+
+        static bool hadError = false;
+
+        private static void report(
+            int line, 
+            String where,
+            String message
+        )
+        {
+            Console.WriteLine(
+                "[line " + line + "] Error" + where + ": " + message
+            );
+
+            hadError = true;
+        }
+
+        private static void run(String source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scanTokens();
+
+            // For now, just print the tokens.
+            foreach(var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+    }
+}
---
+++ TokenType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jclox
+{
+    enum TokenType
+    {
+        // Single-character tokens.
+        LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+        // One or two character tokens.
+        BANG, BANG_EQUAL,
+        EQUAL, EQUAL_EQUAL,
+        GREATER, GREATER_EQUAL,
+        LESS, LESS_EQUAL,
+
+        // Literals.
+        IDENTIFIER, STRING, NUMBER,
+
+        // Keywords.
+        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+        PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+        EOF
+    }
+
+    class Token
+    {
+        TokenType type;
+        string lexeme;
+        Object literal;
+        int line;
+
+        Token(TokenType type, String lexeme, Object literal, int line)
+        {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+
+        override public string ToString()
+        {
+            return type + " " + lexeme + " " + literal;
+        }
+    }
+}
